8d6ae768f9eb657a6bcb429ff27ef8491a9b296c
DNAFun <1925568392@qq.com>
2023-01-05 11:41:16 +0800

    Signed-off-by: DNAFun <1925568392@qq.com>

---
+++ MyDemo/src/com/demo/a/DemoA4.java
@@ -0,0 +1,12 @@
+package com.demo.a;
+
+public class DemoA {
+	public static void main(String[] args) {
+		System.out.println("fix");
+	}
+	
+	class DemoInnerA{
+		String name;
+		String age;
+	}
+}
